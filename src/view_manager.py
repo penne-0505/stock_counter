2a8ac993dfe81a52e3a43b2c675924f6bc17a170
penne-0505 <penne0505pp@gmail.com>
2024-10-09 16:58:32 +0900

    delete fuckin timeout

--- src/view_manager.py
+++ src/view_manager.py
@@ -44,6 +44,6 @@ class DecreaseButton(discord.ui.Button):
 
 class StockManageView(discord.ui.View):
     def __init__(self):
-        super().__init__(timeout=120)
+        super().__init__(timeout=None)
         self.add_item(IncreaseButton())
         self.add_item(DecreaseButton())
